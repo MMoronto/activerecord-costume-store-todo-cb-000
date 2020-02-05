940a6dda8a2a1ef778666c27dd1576543dddde13
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-05 04:09:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,9 +2,10 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
+      t.string :theme
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
