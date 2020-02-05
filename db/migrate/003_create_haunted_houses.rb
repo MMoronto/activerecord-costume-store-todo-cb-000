6517289107b08518f398e276a6369a5b5d2f6578
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-05 04:13:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_time
+      t.datetime :closing_date
       t.text :description
     end
   end
